b3bd98b4915e1642df02a756a866c44aa13bf3c9
NURAENI <nuraeninur2909@gmail.com>
2024-09-04 15:50:18 +0800

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -4,67 +4,22 @@ package day3.javaa;
 public class Day3Javaa {
 
     public static void main(String[] args) {
-        System.out.println("OPERATOR ARITMATIKA :");
-        int a = 10;
-        int b = 5;
-        int c = a + b;
-        System.out.println(c);
-        c = a * b;
-        System.out.println(c);
-        c = a - b;
-        System.out.println(c);
-        c = a / b;
-        System.out.println(c);
-        c = a % b;
-        System.out.println(c);
-        
-        System.out.println("OPERATOR PENUGASAN :");
-        a += b;
-        System.out.println(a);
-        a *= b;
-        System.out.println(a);
-        a -= b;
-        System.out.println(a);
-        a /= b;
-        System.out.println(a);
-        a %= b;
-        System.out.println(a);
-        
-        System.out.println("OPERATOR PERBANDINGAN :");
-        int d = 15;
-        int e = 5;
-        boolean f = d > e;
-        System.out.println(f);
-        boolean w = d < e;
-        System.out.println(w);
-        boolean p = d >= e;
-        System.out.println(p);
-        boolean o = d <= e;
-        System.out.println(o);
-        boolean l = d == e;
-        System.out.println(l);
-        boolean q = d != e;
-        System.out.println(q);
-        
-        System.out.println("OPERATOR LOGIKA :");
-        boolean nu = true;
-        boolean ba = true;
-        boolean ca = nu && ba;
-        System.out.println(ca);
-        boolean ds = nu || ba;
-        System.out.println(ds);
-        boolean la = !nu;
-        System.out.println(la);
-        
-        System.out.println("OPERATOR TERNARRY :");
-        int as = 12;
-        int ad = 6;
-        String zz = as > ad ? "iya" : "tidak";
-        System.out.println(zz);
-        
-        
-        
-        
-    }
-    
-}
+        System.out.print("angka 0-10 : ");
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(i);
+        }
+        System.out.print("\nangka 10-0 : ");
+            for (int j = 10; j >= 0; j--) {
+                System.out.print(j);
+            }
+            System.out.print("\nangka ganjil : ");
+            for (int l = 1; l <= 10; l+=2) {
+                System.out.print(l);
+        }
+            System.out.print("\nangka genap : ");
+            for (int i = 0; i <= 10; i+=2) {
+                System.out.print(i);
+        }
+                
+            }
+                }
