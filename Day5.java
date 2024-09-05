33d849ffa9111a4f497861ed0a5f9f37f8d0e021
NURAENI <nuraeninur2909@gmail.com>
2024-09-06 00:43:28 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,11 @@
+package isc;
+
+public class Day5 {
+    public static void main(String[] args) {
+        /*
+         * System.out.printIn(); yaitu mencetak teks atau output diikuti dengan garis baru 
+         */
+      System.out.println("NURAENI");
+        
+    }
+}
