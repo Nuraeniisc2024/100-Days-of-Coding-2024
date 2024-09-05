1b98a83e4f56ddfec068dfa1632f532212890339
NURAENI <nuraeninur2909@gmail.com>
2024-09-05 16:37:51 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,25 @@
+package isc;
+
+public class Day4 {
+    public static void main(String[] args) {
+        /*System.out.print: mencetak teks tanpa membuat garis baru
+         * contohnya seperti dibawah ini: 
+         */
+        System.out.println("Contoh System.out.print :");
+        System.out.print("NUR");
+        System.out.print("AENI");
+        System.out.println("");
+        /*System.out.printf : mencetak teks dengan format tertentu seperti
+         * %s diunakan untuk mencetak String dan %d untuk mencetak bilangan bulat
+         * contohnya seperti dibawah ini:
+         */
+        
+        System.out.println("Contoh System.out.printf :");
+        String nama = "NUR";
+        int umur = 18;
+        System.out.printf("NAMA : %s, UMUR : %d", nama, umur);
+       
+
+        
+    }
+}
