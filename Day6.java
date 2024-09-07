c96613fa542c33c880215c2a3d4383c148a0edf3
NURAENI <nuraeninur2909@gmail.com>
2024-09-07 11:00:33 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+package isc;
+
+public class Day6 {
+    public static void main(String[] args) {
+        int umur = 18; //variabel yang bertipe data int
+        double beratbadan = 50.1; //variabel yang bertipe data double
+        char kelas = 'D'; //variabel yang bertipe data char
+
+    }
+}
