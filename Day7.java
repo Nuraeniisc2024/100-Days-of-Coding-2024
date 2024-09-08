97d553f5bec4e3ee0379b59671551fb514cf9507
NURAENI <nuraeninur2909@gmail.com>
2024-09-08 13:02:50 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,9 @@
+package isc;
+
+public class Day7 {
+    public static void main(String[] args) {
+        int angkatan = 23; //varibel bertipe data int diinisialisasikan dengan nilai 23
+        char kelas ='D'; //vriabel bertipe data char diinisialisasikan dengan nilai 'A'
+
+    }
+}
