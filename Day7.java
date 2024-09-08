cdbea17e2b164646306faea30e216937836cd4b1
NURAENI <nuraeninur2909@gmail.com>
2024-09-08 13:05:11 +0800

    Update Day7.java

--- Day7.java
+++ Day7.java
@@ -3,7 +3,7 @@ package isc;
 public class Day7 {
     public static void main(String[] args) {
         int angkatan = 23; //varibel bertipe data int diinisialisasikan dengan nilai 23
-        char kelas ='D'; //vriabel bertipe data char diinisialisasikan dengan nilai 'A'
+        char kelas ='D'; //vriabel bertipe data char diinisialisasikan dengan nilai 'D'
 
     }
 }
