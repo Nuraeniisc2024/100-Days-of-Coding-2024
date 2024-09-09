1328db8a6097dfad73b9dbb7a126279f127c8d58
NURAENI <nuraeninur2909@gmail.com>
2024-09-09 11:19:51 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,9 @@
+package isc;
+
+public class Day8 {
+    public static void main(String[] args) {
+        int n = 12;
+        n = 8; // mengubah isi variabel
+        System.out.println(n);
+    }
+}
