5ba47cc101a3e2b28c0ffe2d33c283f8e372311e
NURAENI <nuraeninur2909@gmail.com>
2024-09-12 23:28:55 +0800

    Create Day12.java

---
+++ Day12.java
@@ -0,0 +1,10 @@
+package isc;
+
+public class Day12 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
