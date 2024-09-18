5757c69fe5b5cea6e7bb173ec3e243dca1ff0dd7
NURAENI <nuraeninur2909@gmail.com>
2024-09-18 16:46:17 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,9 @@
+package isc;
+
+public class Day17 {
+    public static void main(String[] args) {
+        char kelas = 'D';
+        char jeniskelamin = 'P';
+        System.out.println("jadi saya kelas " + kelas);
+    }
+}
