377692f8dc08fe67b8f40175740c361299994a0e
NURAENI <nuraeninur2909@gmail.com>
2024-09-18 16:47:53 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,9 @@
+package isc;
+
+public class Day18 {
+    public static void main(String[] args) {
+        boolean pulpen = true;
+        boolean pensil = false;
+        System.out.println(pensil);
+    }
+}
