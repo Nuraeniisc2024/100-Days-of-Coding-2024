5cd7baaf315619916cbb15cae70d36cc4c634640
NURAENI <nuraeninur2909@gmail.com>
2024-09-23 21:00:45 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,11 @@
+package isc;
+
+public class Day22 {
+    public static void main(String[] args) {
+    Integer nur = 12;
+    Integer aeni = 12;
+    System.out.println(nur+aeni);
+    }
+    
+    
+}
