06752e2c4f401a9e077eb60d044779f374db1e96
NURAENI <nuraeninur2909@gmail.com>
2024-09-26 23:36:58 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,10 @@
+package isc;
+
+public class Day26 {
+    public static void main(String[] args) {
+        Character nur = 'A';
+        Character aeni = 'B';
+        System.out.println(aeni);
+
+    }
+}
