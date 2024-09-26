71942507ba32b99af7f976fa3da8939da53d746b
NURAENI <nuraeninur2909@gmail.com>
2024-09-26 23:34:37 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,9 @@
+package isc;
+
+public class Day25 {
+    public static void main(String[] args) {
+        Double nur = 12.2;
+        Double aeni = 12.3;
+        System.out.println(nur+aeni);
+    }
+}
