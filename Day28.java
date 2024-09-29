0d892f6c647867ec7770f4da158b53ea4b49f3fa
NURAENI <nuraeninur2909@gmail.com>
2024-09-29 22:45:05 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,12 @@
+package day28.java;
+
+
+public class Day28Java {
+
+    public static void main(String[] args) {
+        Boolean benar = true;
+        Boolean salah = false;
+        System.out.println("angkatan 23? " + benar);
+    }
+    
+}
