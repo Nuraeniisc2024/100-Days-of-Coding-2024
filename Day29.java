9173a9fcb9f5e5a91a1d85183ae822ab865d3c5c
NURAENI <nuraeninur2909@gmail.com>
2024-09-30 22:09:20 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,14 @@
+package day29.javaa;
+
+import java.math.BigInteger;
+
+public class Day29Javaa {
+
+   
+    public static void main(String[] args) {
+        BigInteger angka = new BigInteger("123456789");
+        System.out.println("angka BigInteger saya adalah :" + angka);
+        
+    }
+    
+}
