ef3c589f5c3db1e4d52d681916ab4eb1aa8e0cfe
NURAENI <nuraeninur2909@gmail.com>
2024-10-02 00:42:30 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,7 @@
+import java.math.BigDecimal;
+public class Day30 {
+    public static void main(String[] args) {
+        BigDecimal angka1 = new BigDecimal("123.45");
+        System.out.println("angka 1 bernilai :" + angka1);
+    }
+}
