ad83c69238836b55a9e2415eba1c16f417a836e7
NURAENI <nuraeninur2909@gmail.com>
2024-10-02 00:42:59 +0800

    Create Day31.java

---
+++ Day31.java
@@ -0,0 +1,7 @@
+public class Day31 {
+    public static void main(String[] args) {
+        String angka = "123";
+        int nilaiAngka = Integer.parseInt(angka);
+System.out.println(nilaiAngka);
+    }
+}
