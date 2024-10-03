9570cc8e88a4f05cf3d3f062ff0b44f383c194b3
NURAENI <nuraeninur2909@gmail.com>
2024-10-03 22:57:14 +0800

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,10 @@
+package isc;
+
+public class Day32 {
+    public static void main(String[] args) {
+        int angka = 12;
+        String Str = Integer.toString(angka);
+        System.out.println(angka);
+        
+    }
+}
