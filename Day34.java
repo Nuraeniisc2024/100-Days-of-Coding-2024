15572b752edb4485baecda211dac640cede6dd38
NURAENI <nuraeninur2909@gmail.com>
2024-10-04 22:52:24 +0800

    Create Day34.java

---
+++ Day34.java
@@ -0,0 +1,11 @@
+package isc;
+
+import java.util.Scanner;
+
+public class Day34 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("MASUKKAN NAMA ANDA :");
+        String nama = input.nextLine();
+    }
+}
