a229e31b6f4c8fe156118b93beac0a3e35cd19f9
NURAENI <nuraeninur2909@gmail.com>
2024-10-05 23:00:28 +0800

    Create Day36.java

---
+++ Day36.java
@@ -0,0 +1,13 @@
+package isc;
+
+public class Day35 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 2;
+        int tambah =  a + b;
+        System.out.println(tambah);
+        int pengurangan = a - b;
+        System.out.println(pengurangan);
+
+    }
+}
