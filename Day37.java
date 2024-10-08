62380c07a316448ae5cb72a338884c4bb57a8c2c
NURAENI <nuraeninur2909@gmail.com>
2024-10-08 10:14:21 +0800

    Update Day37.java

--- Day37.java
+++ Day37.java
@@ -8,7 +8,7 @@ public class Day37 {
         int a = input.nextInt();
         System.out.println("masukkan nilai kedua");
         int b = input.nextInt();
-        System.out.print("hasil perkalian dari variabel a dan b adalah: ");
+        System.out.print("hasil penambahan dari variabel a dan b adalah: ");
         int c = a += b;
         System.out.println(c);
 
@@ -17,7 +17,7 @@ public class Day37 {
         int ab = input.nextInt();
         System.out.println("masukkan nilai kedua");
         int ac = input.nextInt();
-        System.out.print("hasil pembagian dari variabel ab dan ac adalah: ");
+        System.out.print("hasil pemngurangn dari variabel ab dan ac adalah: ");
         int d = ab -= ac;
         System.out.println(d);
     }
