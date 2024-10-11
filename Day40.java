7a21667e5f52c67d34bcd1d3a229db0005ca14b1
NURAENI <nuraeninur2909@gmail.com>
2024-10-11 20:47:56 +0800

    Create Day40.java

---
+++ Day40.java
@@ -0,0 +1,15 @@
+package isc;
+
+public class Day40 {
+    public static void main(String[] args) {
+        System.out.println("variabel a bernilai 10");
+        int a = 10;
+        System.out.println("variabel b bernilai 5");
+        int b = 5;
+        boolean c = a > b;
+        System.out.println("apakah variabel a lebih besar dari b ? " + c);
+        boolean d = a < b;
+        System.out.println("apakah variabel a lebih kecil dari b ? " + d);
+
+    }
+}
