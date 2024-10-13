22da86823a023de34ce5277a09d0533b1471842a
NURAENI <nuraeninur2909@gmail.com>
2024-10-13 19:06:04 +0800

    Create Day42.java

---
+++ Day42.java
@@ -0,0 +1,14 @@
+package isc;
+
+public class Day42 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        boolean c = a == b;
+        System.out.println("apakah variabel a sama dengan variabel b ? " + c);
+        boolean d = a != b;
+        System.out.println("apakah variabel a tidak sama dengan variabel b ? " + d);
+
+
+    }
+}
