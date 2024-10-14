3768b66329a6731d0c6fc158008e25d4abb5070c
NURAENI <nuraeninur2909@gmail.com>
2024-10-15 00:21:59 +0800

    Create Day44.java

---
+++ Day44.java
@@ -0,0 +1,11 @@
+package isc;
+
+public class Day44 {
+    public static void main(String[] args) {
+        boolean pensil = true;
+        boolean pulpen = false;
+        boolean n = pensil && pulpen;
+        System.out.println(n);
+
+    }
+}
