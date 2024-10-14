b20b91bfa57eaeeb6f844947b1e2781f9188b9fe
NURAENI <nuraeninur2909@gmail.com>
2024-10-14 18:50:22 +0800

    Create Day43.java

---
+++ Day43.java
@@ -0,0 +1,12 @@
+package isc;
+
+public class Day43 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        boolean c = a >= b;
+        System.out.println("apakah variabel a lebih besar sama dengan variabel b ? " + c );
+        boolean d = a <= b;
+        System.out.println("apakah variabel a lebih kecil sama dengan variabel b ? " + d );
+    }
+}
