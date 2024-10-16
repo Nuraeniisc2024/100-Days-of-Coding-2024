630f857a572560766364fadad36a8e6a26cf4667
NURAENI <nuraeninur2909@gmail.com>
2024-10-16 21:56:09 +0800

    Create Day45.java

---
+++ Day45.java
@@ -0,0 +1,10 @@
+package isc;
+
+public class Day45 {
+    public static void main(String[] args) {
+        boolean baju = true;
+        boolean celana = false;
+        boolean pilihan = baju || celana;
+        System.out.println(pilihan);
+    }
+}
