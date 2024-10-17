91f00c6d52fdc086f07d4ac294ac464c35db5d22
NURAENI <nuraeninur2909@gmail.com>
2024-10-17 20:11:58 +0800

    Create Day46.java

---
+++ Day46.java
@@ -0,0 +1,9 @@
+package isc;
+
+public class Day46 {
+    public static void main(String[] args) {
+        boolean kebalikan = true;
+        boolean negasi = !kebalikan;
+        System.out.println(negasi);
+    }
+}
