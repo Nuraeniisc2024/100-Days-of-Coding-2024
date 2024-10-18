8439a5a0e3bffe31a363abf13ecdcef65387412e
NURAENI <nuraeninur2909@gmail.com>
2024-10-18 19:50:13 +0800

    Create Day49.java

---
+++ Day49.java
@@ -0,0 +1,15 @@
+package isc;
+
+public class Day49 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        if(a < b){
+            System.out.println("true");
+        }else if(a > b){
+            System.out.println("false");
+        }
+        }
+        }
+    
+
