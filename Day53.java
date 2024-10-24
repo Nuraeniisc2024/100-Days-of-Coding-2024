3ba6f9f15013197c9cabfd1f23d203e94f867d4f
NURAENI <nuraeninur2909@gmail.com>
2024-10-25 00:53:53 +0800

    Create Day53.java

---
+++ Day53.java
@@ -0,0 +1,10 @@
+package isc;
+
+public class Day53 {
+    public static void main(String[] args) {
+        int a = 10;
+        for (int i = 0; i < a; i++) {
+            System.out.println("nur");
+        }
+    }
+}
