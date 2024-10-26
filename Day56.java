92f0f4e608ee31eac81646bc4d0f4375523252a4
NURAENI <nuraeninur2909@gmail.com>
2024-10-26 21:00:51 +0800

    Create Day56.java

---
+++ Day56.java
@@ -0,0 +1,10 @@
+package isc;
+
+public class Day56 {
+    public static void main(String[] args) {
+        for (int x = 5; x > 0; x--) {
+            System.out.println("Nilai x: " + x);
+        }
+        System.out.println("Loop selesai!");
+    }
+}
