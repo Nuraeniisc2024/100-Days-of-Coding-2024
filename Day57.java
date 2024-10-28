a7afb62b0d5bfc852001a9214ebdb81e86691416
NURAENI <nuraeninur2909@gmail.com>
2024-10-28 21:05:40 +0800

    Create Day57.java

---
+++ Day57.java
@@ -0,0 +1,14 @@
+package isc;
+
+public class Day57 {
+    public static void main(String[] args) {
+        int ku = 1;
+
+        while (ku <= 5) {
+            System.out.println("Nilai ku: " + ku);
+            ku++;  // Menambah nilai x sebanyak 1
+        }
+
+        System.out.println("Nilai akhir x: " + ku);
+    }
+}
