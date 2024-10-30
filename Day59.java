8a1a9b334e432778521225e459cd7add19fdf683
NURAENI <nuraeninur2909@gmail.com>
2024-10-30 21:55:56 +0800

    Create Day59.java

---
+++ Day59.java
@@ -0,0 +1,11 @@
+package isc;
+
+public class Day59 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int number : numbers) {
+         System.out.println(number);
+}
+
+    }
+}
