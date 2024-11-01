6f16699f11196700b6895e8b80bd0f22d5ab549d
NURAENI <nuraeninur2909@gmail.com>
2024-11-01 18:53:20 +0800

    Create Day61.java

---
+++ Day61.java
@@ -0,0 +1,12 @@
+package isc;
+
+public class Day61 {
+    public static void main(String[] args) {
+        int i = 1;
+    while (i > 0) {
+    System.out.println("Loop tak berhingga");
+    i++;  // i akan terus bertambah, sehingga kondisinya selalu benar
+}
+
+    }
+}
