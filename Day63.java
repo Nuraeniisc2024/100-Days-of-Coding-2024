9e4bfb67646faac5bf1ffb46b1ed6a6ef05fd5f1
NURAENI <nuraeninur2909@gmail.com>
2024-11-02 22:15:43 +0800

    Create Day63.java

---
+++ Day63.java
@@ -0,0 +1,12 @@
+package isc;
+
+public class Day63 {
+    public static void main(String[] args) {
+                for (int i = 1; i <= 10; i++) {
+                    if (i == 5) {
+                        continue; // Melewatkan iterasi ketika i bernilai 5
+                    }
+                    System.out.println("Angka: " + i);
+                }
+            }
+        }
