1fc3535880b63c006be648065b78fc6183ef052b
NURAENI <nuraeninur2909@gmail.com>
2024-11-06 23:46:09 +0800

    Create Day66.java

---
+++ Day66.java
@@ -0,0 +1,19 @@
+package isc;
+
+public class Day66 {
+   public static void main(String[] args) {
+    int persegi = 3;
+    int persegipanjang = 4;
+    
+    for (int i = 0; i < persegi; i++) {
+        System.out.println(" * * * * ");
+    }
+    System.out.println();
+
+        for (int j = 0; j < persegipanjang ; j++) {
+            System.out.println(" * * * * * * * ");
+            
+        }
+        
+    }
+   }
