e877501b7ce660c3ada825273b94e76a943f70ff
NURAENI <nuraeninur2909@gmail.com>
2024-11-07 23:37:15 +0800

    Create Day67.java

---
+++ Day67.java
@@ -0,0 +1,11 @@
+package isc;
+
+public class Day67 {
+        static void tugasku(){
+            System.out.println("Tadi saya menegerjakan tugas");
+        }
+        
+        public static void main(String[] args) {
+            tugasku();
+    }
+}
