f4e52fa82baf991decc298c8f23c43b68dc29ba0
NURAENI <nuraeninur2909@gmail.com>
2024-11-08 23:56:31 +0800

    Create Day68.java

---
+++ Day68.java
@@ -0,0 +1,18 @@
+package isc;
+
+public class Day68 {
+        public class ContohString {
+
+        
+            public static String getTeks() {
+                return "Halo, Dunia!";
+            }
+        
+            public static void main(String[] args) {
+                              String teks = getTeks();
+        
+                    System.out.println(teks);
+            }
+        }
+        
+    }
