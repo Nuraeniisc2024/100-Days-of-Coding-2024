2b206ea12857318b5b85a32700103965b851da97
NURAENI <nuraeninur2909@gmail.com>
2024-11-10 23:12:09 +0800

    Create Day70.java

---
+++ Day70.java
@@ -0,0 +1,13 @@
+package isc;
+
+public class Day70 {
+    // Method dengan satu parameter string
+    public static void tampilPesan(String pesan) {
+        System.out.println("Pesan: " + pesan);
+    }
+
+    public static void main(String[] args) {
+        // Memanggil method tampilPesan
+        tampilPesan("Halo, selamat datang di Java!");
+}
+}
