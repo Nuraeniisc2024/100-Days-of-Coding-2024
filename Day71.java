04ba180843ec0b229a54fc431a40c6045481965f
NURAENI <nuraeninur2909@gmail.com>
2024-11-12 10:08:33 +0800

    Create Day71.java

---
+++ Day71.java
@@ -0,0 +1,10 @@
+package isc;
+
+public class Day71 {
+        public static void tampilkanPesan(String pesan) {
+            System.out.println("Pesan: " + pesan);
+        }
+        public static void main(String[] args) {
+            tampilkanPesan("Halo, selamat belajar Java!");
+        }
+    }
