638b78c9a760bb6df53d1b3126df40cad987363f
NURAENI <nuraeninur2909@gmail.com>
2024-11-13 20:57:07 +0800

    Create Day73.java

---
+++ Day73.java
@@ -0,0 +1,13 @@
+package isc;
+
+public class Day73 {
+    public class Main {
+        public static void main(String[] args) {
+            String kata = "DonatMart";
+            char karakter = kata.charAt(3); 
+    
+            System.out.println("Karakter pada indeks 3: " + karakter);
+        }
+    }
+    
+}
