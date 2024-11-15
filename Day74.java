5eab2555b5fd04676aebd542bb1848652c7ba4c4
NURAENI <nuraeninur2909@gmail.com>
2024-11-15 23:02:42 +0800

    Create Day74.java

---
+++ Day74.java
@@ -0,0 +1,9 @@
+package isc;
+
+public class Day74 {
+        public static void main(String[] args) {
+            String kalimat = "Halo, selamat datang di Donat Mart!";
+            int panjang = kalimat.length();
+            System.out.println("Panjang kalimat: " + panjang);
+        }
+    }
