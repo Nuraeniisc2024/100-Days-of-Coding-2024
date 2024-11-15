f397d86cc31adc840e2e5e71ebb2bcb8a09b9e55
NURAENI <nuraeninur2909@gmail.com>
2024-11-15 23:03:47 +0800

    Create Day76.java

---
+++ Day76.java
@@ -0,0 +1,11 @@
+package isc;
+
+public class Day76 {
+        public static void main(String[] args) {
+            String teksAsli = "selamat datang di Donat Mart";
+            String teksBesar = teksAsli.toUpperCase();
+  
+            System.out.println("Teks Asli: " + teksAsli);
+            System.out.println("Teks Huruf Besar: " + teksBesar);
+        }
+    }
