533c7ac53fdf21df200d05bfe79cae519e278c13
NURAENI <nuraeninur2909@gmail.com>
2024-11-15 23:03:13 +0800

    Create Day75.java

---
+++ Day75.java
@@ -0,0 +1,11 @@
+package isc;
+
+public class Day75 {
+        public static void main(String[] args) {
+            String teksAsli = "SELAMAT DATANG DI DONAT MART!";
+            String teksKecil = teksAsli.toLowerCase();
+
+            System.out.println("Teks Asli: " + teksAsli);
+            System.out.println("Teks Huruf Kecil: " + teksKecil);
+        }
+    }
