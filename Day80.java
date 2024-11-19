6ef23ca192d72030a49daa6e4418dd74110fa652
NURAENI <nuraeninur2909@gmail.com>
2024-11-19 12:51:52 +0800

    Create Day80.java

---
+++ Day80.java
@@ -0,0 +1,11 @@
+package isc;
+
+public class Day80 {
+public static void main(String[] args) {
+            String teks = "   Donat Mart   ";
+            String hasil = teks.trim();
+            
+            System.out.println("Sebelum trim: \"" + teks + "\"");
+            System.out.println("Setelah trim: \"" + hasil + "\"");
+        }
+    }
