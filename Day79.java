f197b8a8464ab71f32abe8b5098c7acd7a952582
NURAENI <nuraeninur2909@gmail.com>
2024-11-19 12:52:20 +0800

    Create Day79.java

---
+++ Day79.java
@@ -0,0 +1,11 @@
+package isc;
+
+public class Day79 {
+        public static void main(String[] args) {
+            String teks = "Donat Mart";
+
+            String hasil = teks.toString();
+
+            System.out.println("Hasil toString(): " + hasil);
+        }
+    }
