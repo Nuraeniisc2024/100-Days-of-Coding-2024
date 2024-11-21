52573ea0d9904b172ec94ba7719d18dd6e96a14f
NURAENI <nuraeninur2909@gmail.com>
2024-11-21 21:52:44 +0800

    Create Day81.java

---
+++ Day81.java
@@ -0,0 +1,10 @@
+package isc;
+
+public class Day81 {
+        public static void main(String[] args) {
+            double nilai = 3.14;
+            String nilaiString = String.valueOf(nilai);
+    
+            System.out.println("Hasil konversi: " + nilaiString);
+        }
+    }
