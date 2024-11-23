ff63a3312979cdaff5720a1d36d7ca9444994739
NURAENI <nuraeninur2909@gmail.com>
2024-11-23 23:40:59 +0800

    Create Day83.java

---
+++ Day83.java
@@ -0,0 +1,14 @@
+package isc;
+import java.lang.Math;
+public class Day83 {
+    public static void main(String[] args) {
+    double number = 5.75;
+        System.out.println("Angka asli: " + number);
+          // Menggunakan ceil
+        System.out.println("Hasil ceil: " + Math.ceil(number)); // Membulatkan ke atas
+        // Menggunakan floor
+        System.out.println("Hasil floor: " + Math.floor(number)); // Membulatkan ke bawah
+        // Menggunakan round
+        System.out.println("Hasil round: " + Math.round(number)); // Membulatkan ke nilai terdekat
+}   
+}
